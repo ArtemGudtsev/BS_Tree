444c93658da167364aa360f0a1c76de7855793e6
Artem <Artem.Gudtsev@gmail.com>
2015-11-06 15:14:24 +0300

    Add some mocksO

--- Sources/BSTree.Tests/StaticMockFabric.cs
+++ Sources/BSTree.Tests/StaticMockFabric.cs
@@ -8,6 +8,14 @@ namespace BSTree.Tests
         {
             var mock = new Mock<IBSNode<int, string>>();
 
+            mock.Setup(node => node.Insert(1, "1"));
+            mock.Setup(node => node.Insert(2, "2"));
+            mock.Setup(node => node.Insert(0, "0"));
+
+            mock.Setup(node => node.Find(0)).Returns("0");
+            mock.Setup(node => node.Find(1)).Returns("1");
+            mock.Setup(node => node.Find(2)).Returns("2");
+
             return mock;
         }
     }
