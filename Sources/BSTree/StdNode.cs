d963439157df7174d25812e1a7ba027f10e9224f
Artem <Artem.Gudtsev@gmail.com>
2015-11-08 01:36:51 +0300

    Cosmetic changes

--- Sources/BSTree/StdNode.cs
+++ Sources/BSTree/StdNode.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BSTree
+﻿namespace BSTree
 {
     public class StdNode : IBSNode<int, string>
     {
